22a49bee1694776d7edd08c6c0049d21da090610
hanaml0868 <hanaml0868@gmail.com>
2019-02-21 10:09:08 +0700

    เช๊คค่าว่าง

--- app/src/main/java/sdu/cs/amornthep/logintest/MainActivity.java
+++ app/src/main/java/sdu/cs/amornthep/logintest/MainActivity.java
@@ -2,12 +2,41 @@ package sdu.cs.amornthep.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    String nameString,userString,passString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//end class
+
+        //ผูกตัวแปรบน java กับ xml
+        final EditText nameEditText = findViewById(R.id.edtName);
+        final EditText usernameEditText = findViewById(R.id.edtUsername);
+        final EditText passnameEditText = findViewById(R.id.edtPassword);
+        Button loginButton = findViewById(R.id.btnLogin);
+
+        //onClick Login Button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nameString = nameEditText.getText().toString().trim();
+                userString = usernameEditText.getText().toString().trim();
+                passString = passnameEditText.getText().toString().trim();
+
+                //Check ค่าว่าง
+                if ((nameString.length()==0)|| (userString.length()==0)||(passString.length()==0)) {
+                    Toast.makeText(getApplicationContext(),"กรุณากรอกข้อมูลให้ครบทุกช่อง",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });//end setOnClickListener
+
+    }//end onCreate
 }// end class
